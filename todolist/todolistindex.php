2e67a8af68e65c79649ba9b5b98043b90ff2a323
noob59600 <108410583+noob59600@users.noreply.github.com>
2022-07-08 06:08:31 +0200

    Add files via upload

---
+++ todolist/todolistindex.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    
+        <container>
+
+                <header>
+                            <!-- TO DO LIST -->
+                        <div style="text-align:center;">
+                            
+
+                            <div class="d-grid gap-2">
+                                    <button type="button" class="btn btn-warning btn-sm" type="button">DATE 15/06/2022</button>
+                                    <button type="button" class="btn btn-info btn-sm" type="button">M o D i F i E r</button>
+                                    <button type="button" class="btn btn-danger btn-sm" type="button">AFFICHER</button>
+                                    <button type="button" class="btn btn-warning btn-sm" type="button">C R E E R</button>             
+                                    <button type="button" class="btn btn-info btn-sm">S U P P R I M E R</button>
+                            </div>                         
+                        </div>
+                </header>
+                        <div style="text-align:center ;">
+                                <h1> 
+                                        <p class="fs-2">
+                                            
+                                                <span class="align-middle"> 
+                                                    <span class="border border-dark border-4">
+                                                        TO DO L I S T
+                                                    </span> 
+                                                </span>
+                                        </p>
+                                </h1>
+                            <div style="text-align:left ;">
+                                <ul>
+                                    <li>
+                                        <div class="alert alert-dark" role="alert">
+                                         Arrêter le réveil 
+                                        </div>                                        
+                                    </li>
+                                    <li>
+                                    <div class="alert alert-dark" role="alert">
+                                         S' étirer
+                                    </div>          
+                                    </li>
+                                    <li>
+                                    <div class="alert alert-dark" role="alert">
+                                            Plannifier sa journée
+                                    </div>
+                                    </li>
+                                    <li>
+                                    <div class="alert alert-dark" role="alert">
+                                            Se lever
+                                    </div>  
+                                    </li>
+                                    <li>
+                                    <div class="alert alert-dark" role="alert">
+                                            Allumer la cafetière
+                                    </div>   
+                                    </li>
+                                    <li>
+                                    <div class="alert alert-dark" role="alert">
+                                            Attendre
+                                    </div>    
+                                    </li>
+                                    <li>
+                                    <div class="alert alert-dark" role="alert">
+                                            Passer sa journée
+                                    </div>
+                                    </li> 
+                                    <li>
+                                    <div class="alert alert-dark" role="alert">
+                                            E N C O R E
+                                    </div>   
+                                    </li>     
+                                </ul>
+                            </div>
+                        </div>
+        </container>
+   
+</body>
+</html>
